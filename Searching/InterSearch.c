f4a2f85d983fded1f8105902b85c08df1f0dd278
Souvik Pal <spal62588@gmail.com>
2021-07-23 15:09:05 +0530

    Modification & Searching 3rd Problem

--- Searching/InterSearch.c
+++ Searching/InterSearch.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 int search(int *arr, int n, int no)
 {
-	int beg=0;
-	int end=n-1;
-	int pos=-1;
-	while(beg<=end)
+	int beg = 0;
+	int end = n-1;
+	int pos = -1;
+	while(beg <= end)
 	{
-		int mid = (beg+end)/2;
+		int mid = beg + (end-beg) * ((no-*(arr+beg)) / (*(arr+end) - *(arr+beg)));
 		if(no == *(arr+mid))
 		{
-			pos = mid;
+			pos=mid;
 			break;
-		}
-		else if(no > *(arr+mid))
-		{
-			beg = mid + 1;
 		}
 		else if(no < *(arr+mid))
 		{
 			end = mid - 1;
+		} 
+		else if(no > *(arr+mid))
+		{
+			beg = mid + 1;
 		}
 	}
 	return pos;
 }
 int main()
 {
-	printf("Binary Search\n");
-	printf("*************\n\n");
+	printf("Interpolation Search\n");
+	printf("********************\n\n");
 	int arr[100];
 	int n,i,no;
 	printf("Enter Size of Array : ");
 	scanf("%d",&n);
 	printf("\n");
-	printf("Enter Numbers in Sorted Form : \n");
+	printf("Enter Numbers : \n");
 	for(i=0;i<n;i=i+1)
 	{
 		scanf("%d",&arr[i]);
@@ -44,7 +44,7 @@ int main()
 	int pos = search(arr,n,no);
 	if(pos==-1)
 	{
-		printf("Number is not Present in Array\n");
+		printf("Number is Not Present in Array\n");
 	}
 	else
 	{
